206bd4dc6dc121b1ef67cf826c6c920658863268
quentin <quentin@quentin-GS60-2PL>
2018-05-07 15:16:25 +0200

    Boutons retour de tutoriel et crédits

--- src/controleur/ControleurCredits.java
+++ src/controleur/ControleurCredits.java
@@ -1,10 +1,25 @@
 package controleur;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 
 public class ControleurCredits extends ControleurBase
 {    
+    
+    @FXML
+    public Label labelTitre;
+    
+    @FXML
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {        
+        labelTitre.setAlignment(Pos.CENTER);
+    }
+    
     @FXML
     private void retourMenu(ActionEvent event)
     {

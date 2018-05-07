6ba52a7cbe34338c21ed42270135b3b16db41106
quentin <quentin@quentin-GS60-2PL>
2018-05-07 14:48:27 +0200

    FenetreRejoindrePartieReseau

--- src/controleur/ControleurRejoindreReseau.java
+++ src/controleur/ControleurRejoindreReseau.java
@@ -1,13 +1,33 @@
 package controleur;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 
 public class ControleurRejoindreReseau extends ControleurBase
 {    
     @FXML
+    public Label labelTitre;
+    
+    @FXML
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {        
+        labelTitre.setAlignment(Pos.CENTER);
+    }
+    
+    @FXML
     private void clicCommencer(ActionEvent event)
     {
         navigation.changerVue(ControleurJeu.class);
+    }
+    
+    @FXML
+    private void retourMenu(ActionEvent event)
+    {
+        navigation.changerVue(ControleurMenuPrincipal.class);
     }
 }

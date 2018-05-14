80a5c61edfc4575acc236adca5ae423f45cb49d8
Lilian <lilian@Thinkpad>
2018-05-14 18:13:01 +0200

    retour aux sources

--- src/modele/JoueurAAttendre.java
+++
@@ -1,9 +0,0 @@
-package modele;
-
-import java.io.Serializable;
-
-public abstract class JoueurAAttendre extends Joueur implements Serializable
-{
-    public abstract Point getPlacementSuivant();    
-    public abstract Deplacement getDeplacementSuivant();
-}
